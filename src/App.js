c150fa74788c90df45fd44c73d2036843ba6f5cf
subirdas29 <you@example.com>
2022-11-01 20:32:00 +0600

    new added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+// import './App.css';
+
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Routes/Route";
+
+function App() {
+  return (
+    <div>
+  <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
